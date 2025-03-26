1ea0c079eb1ba8cc8a7902829f6caed11c55cdb5
Marc Hanson <marc.hanson.github@gmail.com>
2025-03-26 14:39:25 -0400

    Fix ^ dep

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,10 @@ module.exports = {
   coverageReporters: ['text', 'lcov', 'json-summary'],
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
   },
 }
